f24fbc4e273193085fb3658638b32e4b598f661f
Massil <massil.achab@gmail.com>
2015-11-04 23:28:44 +0100

    ...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # admm-hawkes
-Inference of \ell_1 norm of Hawkes kernels using an ADMM approach
+Inference of $\ell_1$ norm of Hawkes kernels using an ADMM approach
+
+$B^{-1} = O^T D O$
--- updates.py
+++ updates.py
@@ -13,11 +13,11 @@ def update_X3(X2, U3, alpha=0.99):
 def update_X4(Y2B, U5):
     return prox_orthogonality(Y2B + U5)
 
-def update_Y1(X1, Y2, U1, U4, A, C):
-    pass
+def update_Y1(X1, Y2, U1, U4, diagA, C):
+    return np.dot(np.diag(1. / (1. + diagA ** 2)), np.dot(np.diag(diagA),(X1 - U4)) - Y2 + C - U1)
 
-def update_Y2(X4, Y1, U1, U5, B, C):
-    pass
+def update_Y2(X4, Y1, U1, U5, diagD, O, B, C):
+    return np.dot(np.dot(O.T,np.dot(np.diag(1. / (1. + diagD ** 2)),O)),np.dot((X4 - U5),B) - Y1 + C - U1)
 
 def update_U1(U1, Y1, Y2, C):
     return U1 + Y1 + Y2 - C
