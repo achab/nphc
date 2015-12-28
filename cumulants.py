c5a737e9737d513b6a2790510fa060373e9afbf1
Massil <massil.achab@gmail.com>
2015-12-28 14:51:00 +0100

    created class Cumulants, subclass of Estim

--- cumulants.py
+++ cumulants.py
@@ -1,89 +1,28 @@
 import numpy as np
-from numpy.polynomial.legendre import leggauss
-from numba import double, int_, jit
+from transform import empirical_sqrt_mean, empirical_cross_corr, integrated_claw
+from mlpp.hawkesnonparam import Estim
 
 
-class Cumulants:
-    # cumulants is a super class of Estim
-    pass
+class Cumulants(Estim):
 
-# Computation of integral of claw
-def integrated_claw(estim, n_quad=50, xmax=40, method='gauss'):
-    """ (Estim) -> float
-    Computes the integral of the conditional law ij at the
-    difference of quadrature points using linear interpolation
+    def __init__(self):
+        super().__init__()
+        self.L = np.array(self.lam)
+        diagD, O = empirical_cross_corr(self)
+        self.C = np.dot(O,np.dot(np.diag(diagD),O.T))
+        self.K = None
 
-    The following code is borrowed from method `compute` in mlpp/hawkesnoparam/estim.py
-    """
-    # Find closest value of a signal
+    def set_K(self):
+        self.K = ord3cumul(self)
 
-    def lin0(sig, t):
-        (x,y) = sig
-        if (t >= x[-1]): return 0
-        index = np.searchsorted(x,t)
-        if index == len(y)-1: return y[index]
-        if np.abs(x[index]-t) < np.abs(x[index+1]-t):
-            return y[index]
-        else:
-            return y[index+1]
-
-    def linc(sig, t):
-        (x,y) = sig
-        if (t >= x[-1]): return y[-1]
-        index = np.searchsorted(x,t)
-        if np.abs(x[index]-t) < np.abs(x[index+1]-t):
-            return y[index]
-        else:
-            return y[index+1]
-
-    if method == 'gauss':
-        estim.quad_x, estim.quad_w = leggauss(n_quad)
-        estim.quad_x = xmax*(estim.quad_x+1)/2
-        estim.quad_w *= xmax/2
-    elif method == 'lin':
-        x1=np.arange(0.,xmax,xmax/n_quad)
-        estim.quad_x = x1
-        estim.quad_w = estim.quad_x[1:]-estim.quad_x[:-1]
-        estim.quad_w = np.append(estim.quad_w,estim.quad_w[-1])
-        #n_quad = len(estim.quad_x)
-        #estim.quad_x = np.array(estim.quad_x)
-        #estim.quad_w = np.array(estim.quad_w)
-    else:
-        pass
-
-    # Call the function _compute_ints_claw
-    estim._compute_ints_claw()
-
-    # Returns the value of a claw at a point
-    def G(i, j, l, t):
-        if t < 0:
-            print("G(): should not be called for t< 0")
-        index = estim._ijl2index[i][j][l]
-        return lin0(estim._claw[index],t)
-
-    # Returns the integral of a claw between t1 and t2
-    def DIG(i, j, l, t1, t2):
-        if t1 >= t2:
-            print("t2>t1 faux dans IG")
-        index = estim._ijl2index[i][j][l]
-        return linc(estim.IG[index],t2)-linc(estim.IG[index],t1)
-
-    # Returns the integral of x times a claw between t1 and t2
-    def DIG2(i, j, l, t1, t2):
-        if t1 >= t2:
-            print("t2>t1 faux dans IG2")
-        index = estim._ijl2index[i][j][l]
-        return linc(estim.IG2[index],t2)-linc(estim.IG2[index],t1)
-
-    # Fill the matrix to return with integrated claws
-    def fill_matrix_integrated_claw(d):
-        C = np.zeros((d,d))
-        for i in range(d):
-            for j in range(d):
-                C[i][j] = DIG(i, j, 0, 0, xmax)
+    def get_corr(self):
+        G = integrated_claw(self, method='lin')
+        C = np.einsum('i,ij->ij', self.L, G.T)
+        # the following line cancels the edge effects
+        C = .5 * (C + C.T)
         return C
 
-    fast_fill_matrix_integrated_claw = jit(double[:,:](int_))(fill_matrix_integrated_claw)
 
-    return fast_fill_matrix_integrated_claw(estim._dim)
+def ord3cumul(estim):
+    pass
 
--- third_order/loss.py
+++ third_order/loss.py
@@ -1,8 +1,13 @@
 import numpy as np
+from numba import double, int_, jit
 
 def full_tensor(R, L, C):
     d = len(L)
     F = np.zeros((d,d,d))
+    F_1 = np.einsum('im,jm,km->ijk',C,R,R)
+    F += F_1
+    F += np.einsum('jki',F_1)
+    F += np.einsum('kij',F_1)
     F -= 2*np.einsum('m,im,jm,km',L,R,R,R)
     F += np.einsum('im,jm,km->ijk',R,R,C)
     F += np.einsum('im,jm,km->ijk',R,C,R)
@@ -23,3 +28,15 @@ def full_loss(R, L, C, K):
 def partial_loss(R, L, C, K, ind=3):
     G = partial_tensor(R, L, C, ind=ind)
     return .5 * np.sum((G - K)**2) / (d**2)
+
+if __name__ == "__main__":
+    
+    def full_tensor(R, L, C):
+        d = len(L)
+        F_1 = np.einsum('im,jm,km->ijk',C,R,R)
+        F = np.zeros((d,d,d))
+        F += F_1
+        F += np.einsum('jki',F_1)
+        F += np.einsum('kij',F_1)
+        F -= 2*np.einsum('m,im,jm,km',L,R,R,R)
+        return F
