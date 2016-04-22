677ac8c752b6250df22c346bd0f228e48ace44ee
Massil <massil.achab@gmail.com>
2016-04-22 09:41:26 +0200

    changed A_ij in cumulants.py

--- utils/cumulants.py
+++ utils/cumulants.py
@@ -41,31 +41,16 @@ class Cumulants(SimpleHawkes):
         self.hMax = hMax
 
     @autojit
-    def compute_A(self,H=0.):
+    def compute_B(self,H=0.):
         if H == 0.:
             hM = -self.hMax
         else:
             hM = H
         d = self.dim
-        A = np.zeros((d,d))
+        B = np.zeros((d,d))
         for i in range(d):
             for j in range(d):
-                A[i,j] = A_ij(self,i,j,hM)
-        self.A = A
-
-    @autojit
-    def compute_B(self,H=0.):
-        if H == 0.:
-            hM = self.hMax
-        else:
-            hM = H
-        assert self.hMax > 0, "self.hMax should be nonnegative."
-        d = self.dim
-        B = np.zeros((d,d,2))
-        for i in range(d):
-            for j in range(d):
-                B[i,j,0] = F_ijk(self,i,i,j,hM)
-                B[i,j,1] = F_ijk(self,j,i,i,hM)
+                B[i,j] = A_ij(self,i,j,-H,0)
         self.B = B
 
     @autojit
@@ -83,16 +68,39 @@ class Cumulants(SimpleHawkes):
                     F[i,j,k] = F_ijk(self,i,j,k,hM)
         self.F = F
 
+    @autojit
+    def compute_F_c(self,H=0.):
+        if H == 0.:
+            hM = self.hMax
+        else:
+            hM = H
+        assert self.hMax > 0, "self.hMax should be nonnegative."
+        d = self.dim
+        F_c = np.zeros((d,d,2))
+        for i in range(d):
+            for j in range(d):
+                F_c[i,j,0] = F_ijk(self,i,i,j,hM)
+                F_c[i,j,1] = F_ijk(self,j,i,i,hM)
+        self.F_c = F_c
+
+
     def set_R_true(self,R_true):
         self.R_true = R_true
 
+    @autojit
     def set_C(self,H=0.):
         if H == 0.:
             hM = self.hMax
         else:
             hM = H
-            #self.compute_A(hM)
-        self.C = get_C(self.A,self.L,hM)
+        assert self.hMax > 0, "self.hMax should be nonnegative."
+        d = self.dim
+        self.C = np.zeros((d,d))
+        for i in range(d):
+            for j in range(d):
+                self.C[i,j] = A_ij(self,i,j,-hM,hM)
+        # we keep the symmetric part to remove edge effects
+        self.C[:] = 0.5*(self.C + self.C.T)
 
     def set_C_th(self):
         assert self.R_true is not None, "You should provide R_true."
@@ -103,11 +111,8 @@ class Cumulants(SimpleHawkes):
             hM = self.hMax
         else:
             hM = H
-            #self.compute_A(hM)
-            #self.compute_F(hM)
-            #self.set_C(hM)
         assert self.C is not None, "You should first set C using the function 'set_C'."
-        self.K = get_K(self.A,self.F,self.L,self.C,hM)
+        self.K = get_K(self.B,self.F,self.L,self.C,hM)
 
     def set_K_th(self):
         assert self.R_true is not None, "You should provide R_true."
@@ -119,24 +124,24 @@ class Cumulants(SimpleHawkes):
             hM = self.hMax
         else:
             hM = H
-            self.compute_A(hM)
             self.compute_B(hM)
+            self.compute_F_c(hM)
             self.set_C(hM)
-        self.K_part = get_K_part(self.A,self.B,self.L,self.C,hM)
+        self.K_part = get_K_part(self.B,self.F_c,self.L,self.C,hM)
 
     def set_K_part_th(self):
         assert self.R_true is not None, "You should provide R_true."
         self.K_part_th = get_K_part_th(self.L,self.C_th,self.R_true)
 
     def compute_all(self,H=0.):
-        self.compute_A(-H)
+        self.compute_B(-H)
         self.compute_F(H)
         self.set_C(H)
         self.set_K(H)
 
     def compute_all_part(self,H=0.):
-        self.compute_A(-H)
-        self.compute_B(H)
+        self.compute_B(-H)
+        self.compute_F_c(H)
         self.set_C(H)
         self.set_K_part(H)
 
@@ -210,48 +215,43 @@ def get_K_part_th(L,C,R):
     K_part += 2*np.dot(R_*(C-np.dot(R_,np.diag(L))),R_.T)
     return K_part
 
+
 @autojit
-def A_ij(hk,i,j,H):
+def A_ij(cumul,i,j,a,b):
+    """
+
+    Computes the mean number of jumps of N^j between \tau + a and \tau + b, that is
+
+    \frac{1}{T} \sum_{\tau \in Z^i} ( N^j_{\tau + b} - N^j_{\tau + a} - \Lambda^j (b - a) )
+
+    """
     res = 0
     u = 0
     count = 0
-    T_ = hk.time
-    if isinstance(hk,Cumulants):
-        Z_i = hk.N[i]
-        Z_j = hk.N[j]
-    else:
-        Z_i = hk.get_full_process()[i]
-        Z_j = hk.get_full_process()[j]
+    T_ = cumul.time
+    Z_i = cumul.N[i]
+    Z_j = cumul.N[j]
     n_i = len(Z_i)
     n_j = len(Z_j)
-    if H >= 0:
-        for tau in Z_j:
-            while u < n_i and Z_i[u] < tau:
-                u += 1
-            v = u
-            while v < n_i and Z_i[v] < tau + H:
-                v += 1
-            if v < n_i:
-                count += 1
-                res += v-u
-        res -= (i==j)*count
-        if count < n_j:
-            res *= n_j * 1. / count
-    else:
-        for tau in Z_i:
-            while u < n_j and Z_j[u] <= tau:
-                u += 1
-            v = u
-            while v >= 0 and Z_j[v] > tau + H:
-                v -= 1
-            if v >= 0:
-                count += 1
-                res += u-1-v
-        res -= (i==j)*count
-        if count < n_i:
-            res *= n_i * 1. / count
+    assert a < b, "You should provide a and b such that a < b."
+    for tau in Z_i:
+        while u < n_j and Z_j[u] <= tau + a:
+            u += 1
+        #u -= 1
+        v = u
+        while v < n_j and Z_j[v] < tau + b:
+            v += 1
+        #if v < n_j and v > 0:
+        if v < n_j:
+            count += 1
+            res += v-u
+    if count < n_i:
+        res *= n_i * 1. / count
+    res -= count * cumul.L[j] * (b - a)
     res /= T_
     return res
+
+
 
 @autojit
 def F_ijk(hk,i,j,k,H):
@@ -295,28 +295,3 @@ def F_ijk(hk,i,j,k,H):
     return res
 
 
-#old
-
-@autojit
-def moment3_ijk(i,j,k,A_,F_,L,H):
-    res = 0
-    res += (i==j)*(i==k)*L[i]
-    res += F_[i,j,k] - (i==j)*A_[k,i]
-    res += F_[j,k,i] - (k==j)*A_[i,j]
-    res += F_[k,i,j] - (i==k)*A_[j,k]
-    res += (i==j)*(A_[k,i]+A_[i,k])
-    res += (i==k)*(A_[j,i]+A_[i,j])
-    res += (k==j)*(A_[k,i]+A_[i,k])
-    return res
-
-@autojit
-def moment3(A_,F_,L,H):
-    I = np.eye(len(L))
-    M1 = F_.copy()
-    M1 -= np.einsum('jk,ij->ijk',I,A_)
-    M1 += np.einsum('ij,ik->ijk',I,A_+A_.T)
-    M = M1.copy()
-    M += np.einsum('jki',M1)
-    M += np.einsum('kij',M1)
-    M += np.einsum('ij,ik,i->ijk',I,I,L)
-    return M
