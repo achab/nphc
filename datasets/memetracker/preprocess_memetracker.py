9f15338788230505a8508676e5bd4a3d8934fa72
Massil <massil.achab@gmail.com>
2016-05-07 10:15:06 +0200

    ...

--- datasets/memetracker/preprocess_memetracker.py
+++ datasets/memetracker/preprocess_memetracker.py
@@ -24,7 +24,11 @@ def raw2df(filename):
                 df_rows.append(row)
         df = pd.DataFrame(df_rows, columns=['Post_URL', 'Date', 'HyperLink'])
         df['Date'] = pd.to_datetime(df['Date'])
-        df.to_csv("df_"+filename[7:14]+".csv")
+        df = df[['Post_URL', 'Date', 'HyperLink']]
+        df = apply_inplace(df, 'Post_URL', parse_url)
+        df = apply_inplace(df, 'HyperLink', parse_url)
+#        df.to_csv("df_"+filename[7:14]+".csv")
+    return df
 
 def apply_inplace(df, field, fun):
     return pd.concat([df.drop(field, axis=1), df[field].apply(fun)], axis=1)
@@ -33,17 +37,20 @@ def parse_url(url):
     o = urlparse(url)
     return o.scheme + "://" + o.netloc
 
+def split_df_into_links_and_posts(filename):
+    pass
+
 def save_in_one_file():
     import glob
     import os
     list_files = glob.glob('quotes*')
-    path = 'file.h5'
+    path = 'store.h5'
     if os.path.exists(path):
         os.remove(path)
     with pd.get_store(path) as store:
-        for f in list_files:
-            df = pd.read_csv(f)
-            store.append('df',df[['url','count']])
+        for filename in list_files:
+            df = raw2df(filename)
+            store.append('df',df)
 
 
 """
@@ -57,25 +64,5 @@ if __name__ == '__main__':
 """
 
 if __name__ == '__main__':
-    import pandas as pd
-    import numpy as np
-    import os
 
-    files = ['test1.csv','test2.csv']
-    for f in files:
-        pd.DataFrame(np.random.randn(10,2),columns=list('AB')).to_csv(f)
-
-    path = 'test.h5'
-    if os.path.exists(path):
-        os.remove(path)
-
-    with pd.get_store(path) as store:
-        for f in files:
-            df = pd.read_csv(f,index_col=0)
-            try:
-                nrows = store.get_storer('foo').nrows
-            except:
-                nrows = 0
-
-            df.index = pd.Series(df.index) + nrows
-            store.append('foo',df)
+    save_in_one_file()
