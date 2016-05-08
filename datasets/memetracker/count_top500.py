70227b88f0d535b5527f8648dd0e4cfd3dea7ec5
Massil <massil.achab@gmail.com>
2016-05-09 00:18:20 +0200

    should finaly work

--- datasets/memetracker/count_top500.py
+++ datasets/memetracker/count_top500.py
@@ -9,7 +9,8 @@ def worker(filename):
     posts = data['From']
     links = data['To']
     counts = pd.concat([posts,links]).value_counts()
-    counts.to_csv("counts_"+filename[3:],columns=['url','count'])
+    counts.columns = ['url','count']
+    counts.to_csv("counts_"+filename[3:])
 
 if __name__ == "__main__":
     import glob
@@ -18,11 +19,14 @@ if __name__ == "__main__":
     pool = Pool(processes=len(names))
     pool.map(worker, names)
 
-    #counts = glob.glob("counts_*")
-    #frames = []
-    #for filename in counts:
-    #    frames.append(pd.read_csv(filename))
-    #df = pd.concat(frames)
-    #df = df.groupby('url').sum()
-    #df = df.sort_values(by='count',ascending=False)
-    #df[:500].to_csv("top500.csv",header=False)
+    counts = glob.glob("counts_*")
+    frames = []
+    for filename in counts:
+        frames.append(pd.read_csv(filename))
+    df = pd.concat(frames)
+    df.columns = ['url','count']
+    df = df.groupby('url').agg({'count':'sum'})
+    df = pd.concat([pd.DataFrame(df.index),pd.DataFrame(df.values)],axis=1)
+    df.columns = ['url','count']
+    df = df.sort('count',ascending=False)
+    df[:500].to_csv("top500.csv",header=False)
