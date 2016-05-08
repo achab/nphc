cf9f77eda1f0ff03323cf80b58d7e68f2cae95f6
Massil <massil.achab@gmail.com>
2016-05-08 15:40:52 +0200

    added two files to preprocess

---
+++ datasets/memetracker/count_by_df.py
@@ -0,0 +1,19 @@
+from multiprocessing import Pool
+import pandas as pd
+
+def worker(filename):
+    """
+    Return counts by url for the DataFrame in filename
+    """
+    data = pd.DataFrame(filename)
+    posts = data['Post_URL']
+    links = data['HyperLink']
+    counts = posts.add(links).value_counts()
+    counts.to_csv("counts_"+filename[3:])
+
+if __name__ == "__main__":
+    import glob
+    names = glob.glob("df_*")
+
+    pool = Pool(processes=len(names))
+    pool.map(worker, names)
