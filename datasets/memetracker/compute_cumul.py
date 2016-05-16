50f13684fa060b44b0e6b58028516c52795b6e3b
Massil <massil.achab@gmail.com>
2016-05-16 17:48:26 +0200

    added compute_cumul.py

---
+++ datasets/memetracker/compute_cumul.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import glob
+
+list_dir_name = ['top10_1months_start2009-04']
+#list_dir_name = glob.glob('top10_1months*')
+
+for dir_name in list_dir_name:
+
+    print("Starting for dataset in ",dir_name)
+
+    L = glob.glob(dir_name+'/process_*')
+
+    import gzip, pickle
+    N = []
+    for x in L:
+        f = gzip.open(x,'r')
+        process = pickle.load(f,encoding='latin1')
+        f.close()
+        N.append(process)
+
+    from utils.cumulants import Cumulants
+    cumul = Cumulants(N)
+
+    # we set H = 1 hour (in seconds)
+    H = 3600
+    cumul.hMax = H
+    cumul.set_all_part(H)
+
+    ff = gzip.open(dir_name+'/process_with_cumul.pkl.gz','wb')
+    pickle.dump(cumul,ff,protocol=2)
+    ff.close()
