465bfaab0336385d05f6b2956665eed9581618cc
Massil <massil.achab@gmail.com>
2016-05-27 17:46:03 +0200

    added easier way to simulate processes

--- utils/simulate_data.py
+++ utils/simulate_data.py
@@ -1,281 +1,285 @@
-
-# coding: utf-8
-
-# In[6]:
-
 import numpy as np
-import mlpp.pp.hawkes as hk
-from mlpp.base.utils import TimeFunction
-
-
-# ## Read arguments
-
-# In[2]:
-
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("-k",help="Choose a kernel among: 'exp', 'rect' or 'plaw'.",type=str,choices=['exp','rect','plaw'])
-parser.add_argument("-d",help="Choose the dimension of the process: 10 or 100.",type=int,choices=[10,100])
-parser.add_argument("-s",help="Simulate either from a symmetric (1), more complex symmetric (2) or nonsymmetric (0) kernel matrix.",type=int,choices=[0,1,2])
-parser.add_argument("-t",help="log_10 of the length of the simulation ie '3' gives T=1000",type=int,choices=[3,4,5,6,7,8,9,10])
-args = parser.parse_args()
-
-
-# ## Dataset to simulate
-
-# In[3]:
-
-if args.k is None:
-    kernel = 'exp'
-else:
-    kernel = args.k
-
-if args.d is None:
-    d = 10
-else:
-    d = args.d
-
-if args.s is None:
-    symmetric = 1
-else:
-    symmetric = args.s
-
-if args.t is None:
-    T = 1e5
-else:
-    T = 10**args.t
-
-
-# In[4]:
-
-if symmetric == 0:
-    mode = 'd' + str(d) + '_nonsym'
-elif symmetric == 1:
-    mode = 'd' + str(d) + '_sym'
-elif symmetric == 2:
-    mode = 'd' + str(d) + '_sym_hard'
-
-
 
 # ## Major key for exp kernel:
 # ### Since hMax=40 encodes the support, we ensure \beta is not too small for estimation
 # Criterion on exp kernel gives us $\beta_{\min}$ such that $$\exp(-\mbox{hMax} \times \beta_\min) = 10^{-4}$$
 
-# In[2]:
+def args2params(mode, symmetric):
 
-hMax = 40
-from math import log
-beta_min = log(1000) / hMax
-#print("We have")
-#print("    beta_min = ",beta_min)
+    hMax = 40
+    from math import log
+    beta_min = log(1000) / hMax
 
+    if 'd10_sym' in mode:
+        d = 10
+        mu = 0.01 * np.ones(d)
+        Alpha = np.zeros((d,d))
+        Beta = np.zeros((d,d))
+        Alpha[:d/2,:d/2] += 1.
+        Alpha[d/2:,d/2:] += 1.
+        Beta[:d/2,:d/2] += 1000*beta_min
+        Beta[d/2:,d/2:] += 10*beta_min
+        if symmetric == 2:
+            Alpha[6:8,:3] += 3.
+            Beta[6:8,:3] += 100*beta_min
+        Alpha = .5*(Alpha+Alpha.T)
+        Gamma = .5*Alpha
+        Beta = .5*(Beta + Beta.T)
+        Alpha /= 12
 
-# In[3]:
+    elif mode == 'd10_nonsym_1':
+        d = 10
+        mu = 0.01 * np.ones(d)
+        Alpha = np.zeros((d,d))
+        Beta = np.zeros((d,d))
+        for i in range(5):
+            for j in range(5):
+                if i <= j:
+                    Alpha[i][j] = 1.
+                    Beta[i][j] = 1000*beta_min
+        for i in range(5,10):
+            for j in range(5,10):
+                if i >= j:
+                    Alpha[i][j] = 1.
+                    Beta[i][j] = 10*beta_min
+        Gamma = Alpha.copy()
+        Alpha /= 6
 
-#mode = 'd10_sym'
+    elif mode == 'd10_nonsym_2':
+        d = 10
+        mu = 0.01 * np.ones(d)
+        Alpha = np.zeros((d,d))
+        Gamma = np.zeros((d,d))
+        for i in range(5):
+            for j in range(5):
+                if i <= j:
+                    Alpha[i][j] = 1.
+                    Gamma[i][j] = 1000*beta_min
+        for i in range(5,10):
+            for j in range(5,10):
+                if i >= j:
+                    Alpha[i][j] = 1.
+                    Gamma[i][j] = 10*beta_min
+        Gamma *= .1
+        Beta = Alpha.copy()
+        Alpha /= 6
 
+    elif 'd100_sym' in mode:
+        d = 100
+        mu = 0.01 * np.ones(d)
+        Alpha = np.zeros((d,d))
+        Beta = np.zeros((d,d))
+        Alpha[:d/2,:d/2] += 1.
+        Alpha[d/2:,d/2:] += 1.
+        Beta[:d/2,:d/2] += 1000*beta_min
+        Beta[d/2:,d/2:] += 10*beta_min
+        if symmetric == 2:
+            Alpha[60:70,10:20] += 3.
+            Beta[60:70,10:20] += 100*beta_min
+        Alpha = .5*(Alpha+Alpha.T)
+        Beta = .5*(Beta + Beta.T)
+        Gamma = .5*Alpha
+        Alpha /= 120
 
-# In[7]:
+    elif mode == 'd100_nonsym_1':
+        d = 100
+        mu = 0.01 * np.ones(d)
+        Alpha = np.zeros((d,d))
+        Beta = np.zeros((d,d))
+        for i in range(50):
+            for j in range(50):
+                if i <= j:
+                    Alpha[i][j] = 1.
+                    Beta[i][j] = 10*beta_min
+        for i in range(51,80):
+            for j in range(51,80):
+                if i >= j:
+                    Alpha[i][j] = 1.
+                    Beta[i][j] = 100.*beta_min
+        for i in range(81,100):
+            for j in range(81,100):
+                if i <= j:
+                    Alpha[i][j] = 1.
+                    Beta[i][j] = 1000.*beta_min
+        Gamma = Alpha.copy()
+        Alpha /= 40
 
-if 'd10_sym' in mode:
-    d = 10
-    mu = 0.0001 * np.ones(d)
-    Alpha = np.zeros((d,d))
-    Beta = np.zeros((d,d))
-    Gamma = np.zeros((d,d))
-    Alpha[:d/2,:d/2] += 1.
-    Alpha[d/2:,d/2:] += 1.
-    Beta[:d/2,:d/2] += 1000*beta_min
-    Beta[d/2:,d/2:] += 10*beta_min
-    # add noise
-    #Alpha += 0.01
-    if symmetric == 2:
-        Alpha[6:8,:3] += 3.
-        Beta[6:8,:3] += 100*beta_min
-    Alpha = .5*(Alpha+Alpha.T)
-    Gamma = .5*Alpha
-    Beta = .5*(Beta + Beta.T)
-    Alpha /= 12
+    elif mode == 'd100_nonsym_2':
+        d = 100
+        mu = 0.01 * np.ones(d)
+        Alpha = np.zeros((d,d))
+        Gamma = np.zeros((d,d))
+        for i in range(50):
+            for j in range(50):
+                if i <= j:
+                    Alpha[i][j] = 1.
+                    Gamma[i][j] = 10*beta_min
+        for i in range(51,80):
+            for j in range(51,80):
+                if i >= j:
+                    Alpha[i][j] = 1.
+                    Gamma[i][j] = 100.*beta_min
+        for i in range(81,100):
+            for j in range(81,100):
+                if i <= j:
+                    Alpha[i][j] = 1.
+                    Gamma[i][j] = 1000.*beta_min
+        Gamma *= .1
+        Beta = Alpha.copy()
+        Alpha /= 40
 
-elif mode == 'd10_nonsym':
-    d = 10
-    mu = 0.0001 * np.ones(d)
-    Alpha = np.zeros((d,d))
-    Beta = np.zeros((d,d))
-    Gamma = np.zeros((d,d))
-    for i in range(5):
-        for j in range(5):
-            if i <= j:
-                Alpha[i][j] = 1.
-                Beta[i][j] = 1000*beta_min
-    for i in range(5,10):
-        for j in range(5,10):
-            if i >= j:
-                Alpha[i][j] = 1.
-                Beta[i][j] = 10*beta_min
-    Gamma = .5*Alpha
-    Alpha /= 6
-
-elif 'd100_sym' in mode:
-    d = 100
-    mu = 0.0001 * np.ones(d)
-    Alpha = np.zeros((d,d))
-    Beta = np.zeros((d,d))
-    Gamma = np.zeros((d,d))
-    Alpha[:d/2,:d/2] += 1.
-    Alpha[d/2:,d/2:] += 1.
-    Beta[:d/2,:d/2] += 1000*beta_min
-    Beta[d/2:,d/2:] += 10*beta_min
-    #add noise
-    #Alpha += 0.01
-    if symmetric == 2:
-        Alpha[60:70,10:20] += 3.
-        Beta[60:70,10:20] += 100*beta_min
-    Alpha = .5*(Alpha+Alpha.T)
-    Beta = .5*(Beta + Beta.T)
-    Gamma = .5*Alpha
-    Alpha /= 120
-
-elif mode == 'd100_nonsym':
-    d = 100
-    mu = 0.0001 * np.ones(d)
-    Alpha = np.zeros((d,d))
-    Beta = np.zeros((d,d))
-    Gamma = np.zeros((d,d))
-    for i in range(40):
-        for j in range(40):
-            if i <= j:
-                Alpha[i][j] = 1.
-                Beta[i][j] = 10*beta_min
-    for i in range(41,60):
-        for j in range(41,60):
-            Alpha[i][j] = 1.
-            Beta[i][j] = (100+900*np.random.rand())*100.*beta_min
-    for i in range(61,100):
-        for j in range(61,100):
-            if i >= j:
-                Alpha[i][j] = 1.
-                Beta[i][j] = 100.*beta_min
-    Gamma = .5*Alpha
-    Alpha /= 30
-
-
-# ## Simulation of the process
-
-# ### Design rectangular kernel
-
-# In[ ]:
-
-Phi_norm = np.zeros_like(Alpha)
-if kernel == 'exp':
-    kernels = [[hk.HawkesKernelExp(a, b) for (a, b) in zip(a_list, b_list)] for (a_list, b_list) in zip(Alpha, Beta)]
-    Phi_norm = Alpha.copy()
-
-elif kernel == 'plaw':
-    def kernel_plaw(alpha,beta,gamma,support=-1):
-        """
-        Alternative definition.
-        phi(t) = alpha * beta / (1 + beta t) ** (1 + gamma)
-        """
-        if beta > 0:
-            return hk.HawkesKernelPowerLaw(alpha/(beta**gamma),1./beta,1.+gamma,support)
-        else:
-            return hk.HawkesKernelPowerLaw(0.,1.,1.,support)
-    kernels = [[kernel_plaw(a*g, b, g, -1) for (a, b, g) in zip(a_list, b_list, g_list)] for (a_list, b_list, g_list) in zip(Alpha, Beta, Gamma)]
-    Phi_norm = Alpha.copy()
-
-elif kernel == 'rect':
-    def kernel_rect(alpha, beta, gamma):
-        if beta > 0:
-            T = np.array([0, gamma, gamma + 1./beta ], dtype=float)
-            Y = np.array([0, alpha*beta,0], dtype=float)
-            tf = TimeFunction([T, Y], inter_mode=TimeFunction.InterConstRight,dt=0.0001)
-            return hk.HawkesKernelTimeFunc(tf)
-        else:
-            T = np.array([0, 1, 1.5 ], dtype=float)
-            Y = np.array([0, 0, 0], dtype=float)
-            tf = TimeFunction([T, Y], inter_mode=TimeFunction.InterConstRight)
-            return hk.HawkesKernelTimeFunc(tf)
-    kernels = [[kernel_rect(a, b, g) for (a, b, g) in zip(a_list, b_list, g_list)] for (a_list, b_list, g_list) in zip(Alpha, Beta, Gamma)]
-    Phi_norm = Alpha.copy()
+    return mu, Alpha, Beta, Gamma
 
 
-# ### Check if $||\Phi||$, can generate a stable Hawkes process
+def params2kernels(kernel, Alpha, Beta, Gamma):
 
-# In[ ]:
+    import mlpp.pp.hawkes as hk
+    from mlpp.base.utils import TimeFunction
 
-_, s, _ = np.linalg.svd(Phi_norm)
-print("Max singular value = ",s.max())
-assert s.max() < 1, "Phi_norm cannot generate a stable Hawkes process"
+    if kernel == 'exp':
+        kernels = [[hk.HawkesKernelExp(a, b) for (a, b) in zip(a_list, b_list)] for (a_list, b_list) in zip(Alpha, Beta)]
 
+    elif kernel == 'plaw':
+        def kernel_plaw(alpha,beta,gamma,support=-1):
+            """
+            Alternative definition.
+            phi(t) = alpha * beta / (1 + beta t) ** (1 + gamma)
+            """
+            if beta > 0:
+                return hk.HawkesKernelPowerLaw(alpha/(beta**gamma),1./beta,1.+gamma,support)
+            else:
+                return hk.HawkesKernelPowerLaw(0.,1.,1.,support)
+        kernels = [[kernel_plaw(a*g, b, g, -1) for (a, b, g) in zip(a_list, b_list, g_list)] for (a_list, b_list, g_list) in zip(Alpha, Beta, Gamma)]
 
-# In[35]:
+    elif kernel == 'rect':
+        def kernel_rect(alpha, beta, gamma):
+            if beta > 0:
+                T = np.array([0, gamma, gamma + 1./beta ], dtype=float)
+                Y = np.array([0, alpha*beta,0], dtype=float)
+                tf = TimeFunction([T, Y], inter_mode=TimeFunction.InterConstRight,dt=0.0001)
+                return hk.HawkesKernelTimeFunc(tf)
+            else:
+                T = np.array([0, 1, 1.5 ], dtype=float)
+                Y = np.array([0, 0, 0], dtype=float)
+                tf = TimeFunction([T, Y], inter_mode=TimeFunction.InterConstRight)
+                return hk.HawkesKernelTimeFunc(tf)
+        kernels = [[kernel_rect(a, b, g) for (a, b, g) in zip(a_list, b_list, g_list)] for (a_list, b_list, g_list) in zip(Alpha, Beta, Gamma)]
 
-h = hk.Hawkes(kernels=kernels, mus=list(mu))
-h.simulate(T)
-
-
-# ## Use the class Cumulants
-
-# In[43]:
-
-from utils.cumulants import Cumulants
-N = h.get_full_process()
-cumul = Cumulants(N,hMax=16)
-# compute everything
-cumul.set_all()
-from scipy.linalg import inv
-R_true = inv(np.eye(d)-Phi_norm)
-cumul.set_R_true(R_true)
-cumul.set_all_part()
-cumul.set_all()
-
-
-# In[44]:
-
-from utils.metrics import rel_err
-print("rel_err on C = ", rel_err(cumul.C_th,cumul.C))
-print("rel_err on K = ", rel_err(cumul.K_th,cumul.K))
-print("rel_err on K_part = ", rel_err(cumul.K_part_th,cumul.K_part))
+    return kernels
 
 
-# In[37]:
+def simulate_and_compute_cumul(mu, kernels, Alpha, T, hM=20):
+    import mlpp.pp.hawkes as hk
+    h = hk.Hawkes(kernels=kernels, mus=list(mu))
+    h.simulate(T)
+    # use the class Cumulants
+    from utils.cumulants import Cumulants
+    N = h.get_full_process()
+    cumul = Cumulants(N,hMax=hM)
+    # compute everything
+    from scipy.linalg import inv
+    d = Alpha.shape[0]
+    R_true = inv(np.eye(d)-Alpha)
+    cumul.set_R_true(R_true)
+    cumul.set_all_part()
+    cumul.set_C_th()
+    cumul.set_K_part_th()
 
-with_Beta = True
-without_N = False
+    from nphc.utils.metrics import rel_err
+    print("rel_err on C = ", rel_err(cumul.C_th,cumul.C))
+    print("rel_err on K = ", rel_err(cumul.K_th,cumul.K))
+    print("rel_err on K_part = ", rel_err(cumul.K_part_th,cumul.K_part))
 
-from math import log10
-name = kernel + '_' + mode + '_log10T' + str(int(log10(T)))
+    return cumul
 
-if with_Beta and without_N:
-    cumul.N = None
-    data = (cumul,Beta)
+
+def save(cumul, Alpha, Beta, Gamma, kernel, mode, T, with_params=True, without_N=False, suffix=''):
+
+    from math import log10
     import gzip, pickle
-    f = gzip.open('.../datasets/' + name + '_with_Beta_without_N.pkl.gz','wb')
-    pickle.dump(data, f, protocol=2)
-    f.close()
-    cumul.N = h.get_full_process()
+    name = kernel + '_' + mode + '_log10T' + str(int(log10(T)))
 
-elif with_Beta and not without_N:
-    data = (cumul,Beta)
-    import gzip, pickle
-    f = gzip.open('.../datasets/' + name + '_with_Beta.pkl.gz','wb')
-    pickle.dump(data, f, protocol=2)
-    f.close()
+    # Create folders if they don't exist yet
+    dir_name = '../datasets/' + kernel
+    import os
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
 
-elif not with_Beta and without_N:
-    cumul.N = None
-    import gzip, pickle
-    f = gzip.open('.../datasets/' + name + '_without_N.pkl.gz','wb')
-    pickle.dump(cumul, f, protocol=2)
-    f.close()
-    cumul.N = h.get_full_process()
+    if with_params and without_N:
+        tmp = cumul.N.copy()
+        cumul.N = None
+        data = (cumul,Alpha,Beta,Gamma)
+        f = gzip.open(dir_name + '/' + name + '_with_params_without_N' + suffix + '.pkl.gz','wb')
+        pickle.dump(data, f, protocol=2)
+        f.close()
+        cumul.N = tmp
 
-elif not with_Beta and not without_N:
-    import gzip, pickle
-    f = gzip.open('.../datasets/' + name + '.pkl.gz','wb')
-    pickle.dump(cumul, f, protocol=2)
-    f.close()
+    elif with_params and not without_N:
+        data = (cumul,Alpha,Beta,Gamma)
+        f = gzip.open(dir_name + '/' + name + '_with_params' + suffix + '.pkl.gz','wb')
+        pickle.dump(data, f, protocol=2)
+        f.close()
+
+    elif not with_params and without_N:
+        tmp = cumul.N.copy()
+        cumul.N = None
+        f = gzip.open(dir_name + '/' + name + '_without_N' + suffix + '.pkl.gz','wb')
+        pickle.dump(cumul, f, protocol=2)
+        f.close()
+        cumul.N = tmp
+
+    elif not with_params and not without_N:
+        f = gzip.open(dir_name + '/' + name + suffix + '.pkl.gz','wb')
+        pickle.dump(cumul, f, protocol=2)
+        f.close()
 
 
-# In[ ]:
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k",help="Choose a kernel among: 'exp', 'rect' or 'plaw'.",type=str,choices=['exp','rect','plaw'])
+    parser.add_argument("-d",help="Choose the dimension of the process: 10 or 100.",type=int,choices=[10,100])
+    parser.add_argument("-s",help="Simulate either from a symmetric (2), more complex symmetric (3), nonsymmetric 1 (0) or nonsymmetric 2 (1) kernel matrix.",type=int,choices=[0,1,2,3])
+    parser.add_argument("-t",help="log_10 of the length of the simulation ie '3' gives T=1000",type=int,choices=[3,4,5,6,7,8,9,10])
+    args = parser.parse_args()
+
+
+    ## Parse arguments
+
+    if args.k is None:
+        kernel = 'exp'
+    else:
+        kernel = args.k
+
+    if args.d is None:
+        d = 10
+    else:
+        d = args.d
+
+    if args.s is None:
+        symmetric = 1
+    else:
+        symmetric = args.s
+
+    if args.t is None:
+        T = 1e5
+    else:
+        T = 10**args.t
+
+    if symmetric == 0:
+        mode = 'd' + str(d) + '_nonsym_1'
+    elif symmetric == 1:
+        mode = 'd' + str(d) + '_nonsym_2'
+    elif symmetric == 2:
+        mode = 'd' + str(d) + '_sym'
+    elif symmetric == 3:
+        mode = 'd' + str(d) + '_sym_hard'
+
+    mu, Alpha, Beta, Gamma = args2params(mode, symmetric)
+
+    kernels = params2kernels(kernel, Alpha, Beta, Gamma)
+
+    cumul = simulate_and_compute_cumul(mu, kernels, Alpha, T, 20)
+
+    save(cumul, Alpha, Beta, Gamma, kernel, mode, T)
---
+++ utils/simulate_multiple_processes.py
@@ -0,0 +1,41 @@
+from multiprocessing import Pool
+from utils.simulate_data import args2params, params2kernels, simulate_and_compute_cumul, save
+import numpy as np
+
+def ix2str(ix):
+    if ix < 10:
+        ix_str = '00' + str(ix)
+    elif ix < 100:
+        ix_str = '0' + str(ix)
+    else:
+        ix_str = str(ix)
+    return ix_str
+
+symmetric = 0
+kernel = 'exp'
+d = 100
+T = 1e8
+
+if symmetric == 0:
+    mode = 'd' + str(d) + '_nonsym_1'
+elif symmetric == 1:
+    mode = 'd' + str(d) + '_nonsym_2'
+elif symmetric == 2:
+    mode = 'd' + str(d) + '_sym'
+elif symmetric == 3:
+    mode = 'd' + str(d) + '_sym_hard'
+
+
+def worker(ix):
+    ix_str = ' ' + ix2str(ix)
+    mu, Alpha, Beta, Gamma = args2params(mode, symmetric)
+    kernels = params2kernels(kernel, Alpha, Beta, Gamma)
+    cumul = simulate_and_compute_cumul(mu, kernels, Alpha, T, 20)
+    save(cumul, Alpha, Beta, Gamma, kernel, mode, T, suffix=ix_str)
+
+if __name__ == '__main__':
+
+    indices = np.arange(100)
+    pool = Pool()
+    pool.map(worker,indices)
+
