fixed tf and float precision related bug

main.py
@@ -10,9 +10,9 @@ def NPHC(cumulants, starting_point, alpha=.5, training_epochs=1000, learning_rat
 
     d = cumulants.dim
 
-    L = tf.placeholder('float', d, name='L')
-    C = tf.placeholder('float', (d,d), name='C')
-    K_c = tf.placeholder('float', (d,d), name='K_c')
+    L = tf.placeholder(tf.float32, d, name='L')
+    C = tf.placeholder(tf.float32, (d,d), name='C')
+    K_c = tf.placeholder(tf.float32, (d,d), name='K_c')
     if stochastic:
         ind_i = tf.placeholder(tf.int32, shape=[1], name='ind_i')
         ind_j = tf.placeholder(tf.int32, shape=[1], name='ind_j')
@@ -21,8 +21,6 @@ def NPHC(cumulants, starting_point, alpha=.5, training_epochs=1000, learning_rat
         C_j = tf.gather(C, ind_j)
         C_ij = tf.gather_nd(C, ind_ij)
         K_c_ij = tf.gather_nd(K_c, ind_ij)
-        W_2_ij = tf.gather_nd(cumulants.W_2, ind_ij)
-        W_3_ij = tf.gather_nd(cumulants.W_3, ind_ij)
 
     R = tf.Variable(starting_point, name='R')
     if stochastic:
@@ -30,28 +28,27 @@ def NPHC(cumulants, starting_point, alpha=.5, training_epochs=1000, learning_rat
         R_j = tf.gather(R, ind_j)
 
     # Set weight matrices
-    if alpha > 0:
-        cumulants.set_W_2(starting_point)
-    if alpha < 1:
-        cumulants.set_W_3(starting_point)
+    cumulants.set_W_2(starting_point)
+    cumulants.set_W_3(starting_point)
+    if stochastic:
+        W_2_ij = tf.gather_nd(cumulants.W_2, ind_ij)
+        W_3_ij = tf.gather_nd(cumulants.W_3, ind_ij)
 
     # Construct model
-    activation_3 = tf.sub(tf.add(tf.matmul(tf.square(R),C,transpose_b=True), tf.matmul(tf.scalar_mul(2.0,tf.mul(R,C)),R,transpose_b=True)), \
-                          tf.matmul(tf.scalar_mul(2.0,tf.square(R)),tf.matmul(tf.diag(L),R,transpose_b=True)))
+    activation_3 = tf.matmul(tf.square(R),C,transpose_b=True) + 2.0*tf.matmul(tf.mul(R,C),R,transpose_b=True) - 2.0*tf.matmul(tf.square(R),tf.matmul(tf.diag(L),R,transpose_b=True))
     activation_2 = tf.matmul(R,tf.matmul(tf.diag(L),R,transpose_b=True))
 
     if stochastic:
-        act_3_ij = tf.reduce_sum( tf.sub( tf.add( tf.mul( C_j, tf.square( R_i) ) , \
-                                                  tf.scalar_mul(2.0, tf.mul( tf.mul( R_i, C_i ), R_j ) ) ),\
-                                          tf.scalar_mul(2.0, tf.mul( tf.mul( L, R_j ), tf.square( R_i ) ) ) ) )
-        act_2_ij = tf.reduce_sum( tf.mul( tf.mul( L, R_i ), R_j ) )
-        tot_cost = tf.add( tf.scalar_mul( 1-alpha, tf.reduce_mean( tf.squared_difference( activation_3, K_c ) ) ), \
-                           tf.scalar_mul( alpha, tf.reduce_mean( tf.squared_difference( activation_2, C ) ) ) )
-        cost = tf.add( tf.reduce_mean( tf.truediv( tf.squared_difference( act_3_ij, K_c_ij ), W_3_ij) ), \
-                       tf.scalar_mul( alpha, tf.reduce_mean( tf.truediv( tf.squared_difference( act_2_ij, C_ij ), W_2_ij) ) ) )
+        act_3_ij = tf.reduce_sum( tf.mul( C_j, tf.square( R_i) ) + 2.0*tf.mul( tf.mul( R_i, C_i ), R_j ) - 2.0*tf.mul( tf.mul( L, R_j ), tf.square( R_i ) ) )
+        act_2_ij = tf.reduce_sum( tf.mul( tf.mul( tf.cast(L,tf.float32), R_i ), R_j ) )
+        tot_cost = (1-alpha) * tf.reduce_mean(tf.truediv( tf.squared_difference( activation_3, K_c ), tf.cast(cumulants.W_3,tf.float32)) ) + alpha * tf.reduce_mean( tf.truediv( tf.squared_difference( activation_2, C ) , tf.cast(cumulants.W_2,tf.float32) ) )
+        cost =  (1-alpha) * tf.reduce_mean( tf.truediv( tf.squared_difference( act_3_ij, K_c_ij ), tf.cast(W_3_ij,tf.float32)) ) + alpha * tf.reduce_mean( tf.truediv( tf.squared_difference( act_2_ij, C_ij ), tf.cast(W_2_ij,tf.float32)) )
+        tot_cost = tf.cast(tot_cost, tf.float32)
+        cost = tf.cast(cost, tf.float32)
     else:
         cost = tf.add( tf.scalar_mul( 1-alpha, tf.reduce_mean( tf.truediv( tf.squared_difference( activation_3, K_c ), cumulants.W_3) ) ), \
                        tf.scalar_mul( alpha, tf.reduce_mean( tf.truediv( tf.squared_difference( activation_2, C ) , cumulants.W_2) ) ) )
+        cost = tf.cast(cost, tf.float32)
 
 
     if optimizer == 'momentum':