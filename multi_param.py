671d6e9c9cde9104ceff6f7526ac8d69d7aa3d64
Massil <massil.achab@gmail.com>
2016-06-15 16:18:48 +0200

    added multi_nphc.py and multi_param.py

--- multi_param.py
+++ multi_param.py
@@ -1,27 +1,31 @@
+from mlpp.optim.model import ModelHawkesFixedBeta
+from mlpp.pp import HawkesKernelExp
+from scipy.optimize import minimize
 from multiprocessing import Pool
-from scipy.linalg import sqrtm
-import tensorflow as tf
-from main import NPHC
+import gzip, pickle
 import numpy as np
 
 def worker(kernel_mode_log10T,learning_rate=10.,training_epochs=1000,display_step=200):
 
     kernel, mode, log10T = kernel_mode_log10T
-    url = 'https://s3-eu-west-1.amazonaws.com/nphc-data/{}_{}_log10T{}_with_params_without_N.pkl.gz'.format(kernel, mode, log10T)
-    from utils.loader import load_data
-    cumulants = load_data(url)[0]
+    i = kernel.find('_d10')
+    filename = 'datasets/' + kernel[:i] + '/{}_{}_log10T{}_with_params_000.pkl.gz'
+    f = gzip.open(filename, 'rb')
+    data = pickle.load(f)
+    f.close()
 
-    d = cumulants.dim
+    cumul, Alpha, Beta, Gamma = data
 
-    # Starting point
-    sqrt_C = sqrtm(cumulants.C)
-    sqrt_L = np.sqrt(cumulants.L)
-    initial = tf.constant(np.dot(sqrt_C,np.diag(1./sqrt_L)).astype(np.float32),shape=[d,d])
+    ticks = cumul.N
+    d = cumul.dim
 
-    R = NPHC(cumulants,initial,alpha=0.5,training_epochs=training_epochs,stochastic=False,weightGMM='eye',\
-         display_step=display_step,learning_rate=learning_rate,optimizer='adam')
+    model = ModelHawkesFixedExpKernLogLik(beta).fit(ticks)
+    bnds = [(.01, None) for _ in range(model.n_coeffs)]
+    result = minimize(lambda x: model.loss(x), 1*np.ones(model.n_coeffs), method='L-BFGS-B', bounds=bnds)
+    pred_G = result['x'][d:].reshape(d,d)
 
-    return R
+    return pred_G
+
 
 if __name__ == '__main__':
 
