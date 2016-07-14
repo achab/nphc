85f3dedd7fb4dabc42be17e10f9e1e8a5a220992
Massil <massil.achab@gmail.com>
2016-07-14 15:38:11 +0200

    remove useless arguments of functions

--- utils/cumulants.py
+++ utils/cumulants.py
@@ -50,12 +50,12 @@ class Cumulants(object):
         self.F_c = np.zeros((d,d))
         for i in range(d):
             for j in range(d):
-                self.F_c[i,j] = 2 * ( E_ijk(self.N[j],self.N[i],self.N[j],-hM,hM,-hM,hM,self.time,self.L[j],self.L[i],self.L[j]) /\
-                                      - self.L[j]*( 2*hM*A_ij(self.N[i],self.N[j],-2*hM,2*hM,self.time,self.L[i],self.L[j]) /\
-                                                    - 2*I_ij(self.N[i],self.N[j],2*hM,self.time,self.L[i],self.L[j]) ) )
-                self.F_c[i,j] += E_ijk(self.N[j],self.N[j],self.N[i],-hM,hM,-hM,hM,self.time,self.L[j],self.L[j],self.L[i]) /\
-                                 - self.L[i]*( 2*hM*A_ij(self.N[j],self.N[j],-2*hM,2*hM,self.time,self.L[j],self.L[j]) /\
-                                              - 2*I_ij(self.N[j],self.N[j],2*hM,self.time,self.L[j],self.L[j]))
+                self.F_c[i,j] = 2 * ( E_ijk(self.N[j],self.N[i],self.N[j],-hM,hM,self.time,self.L[j],self.L[i]) /\
+                                      - self.L[j]*( 2*hM*A_ij(self.N[i],self.N[j],-2*hM,2*hM,self.time,self.L[j]) /\
+                                                    - 2*I_ij(self.N[i],self.N[j],2*hM,self.time,self.L[j]) ) )
+                self.F_c[i,j] += E_ijk(self.N[j],self.N[j],self.N[i],-hM,hM,self.time,self.L[j],self.L[j]) /\
+                                 - self.L[i]*( 2*hM*A_ij(self.N[j],self.N[j],-2*hM,2*hM,self.time,self.L[j]) /\
+                                              - 2*I_ij(self.N[j],self.N[j],2*hM,self.time,self.L[j]))
         self.F_c /= 3
 
     #@autojit
@@ -69,12 +69,12 @@ class Cumulants(object):
             self.C = np.zeros((d,d))
             for i in range(d):
                 for j in range(d):
-                    self.C[i,j] = A_ij(self.N[i],self.N[j],-hM,hM,self.time,self.L[i],self.L[j])
+                    self.C[i,j] = A_ij(self.N[i],self.N[j],-hM,hM,self.time,self.L[j])
         elif method == 'parallel':
-            l = Parallel(-1)(delayed(A_ij)(self.N[i],self.N[j],-hM,hM,self.time,self.L[i],self.L[j]) for i in range(d) for j in range(d))
+            l = Parallel(-1)(delayed(A_ij)(self.N[i],self.N[j],-hM,hM,self.time,self.L[j]) for i in range(d) for j in range(d))
             self.C = np.array(l).reshape(d,d)
         # we keep the symmetric part to remove edge effects
-        self.C[:] = 0.5*(self.C + self.C.T)
+        self.C[:] = 0.5 * (self.C + self.C.T)
 
     #@autojit
     def set_J(self, H=0.,method='parallel'):
@@ -87,9 +87,9 @@ class Cumulants(object):
             self.J = np.zeros((d,d))
             for i in range(d):
                 for j in range(d):
-                    self.J[i,j] = I_ij(self.N[i],self.N[j],hM,self.time,self.L[i],self.L[j])
+                    self.J[i,j] = I_ij(self.N[i],self.N[j],hM,self.time,self.L[j])
         elif method == 'parallel':
-            l = Parallel(-1)(delayed(I_ij)(self.N[i],self.N[j],hM,self.time,self.L[i],self.L[j]) for i in range(d) for j in range(d) )
+            l = Parallel(-1)(delayed(I_ij)(self.N[i],self.N[j],hM,self.time,self.L[j]) for i in range(d) for j in range(d) )
             self.J = np.array(l).reshape(d,d)
         # we keep the symmetric part to remove edge effects
         self.J[:] = 0.5 * (self.J + self.J.T)
@@ -97,19 +97,19 @@ class Cumulants(object):
     #@autojit
     def set_E_c(self,H=0.,method='parallel'):
         if H == 0.:
-            hM = .5*self.hMax
+            hM = self.hMax
         else:
-            hM = .5*H
+            hM = H
         d = self.dim
         if method == 'classic':
             self.E_c = np.zeros((d,d,2))
             for i in range(d):
                 for j in range(d):
-                    self.E_c[i,j,0] = E_ijk(self.N[i],self.N[j],self.N[j],-hM,hM,-hM,hM,self.time,self.L[i],self.L[j],self.L[j])
-                    self.E_c[i,j,1] = E_ijk(self.N[j],self.N[j],self.N[i],-hM,hM,-hM,hM,self.time,self.L[j],self.L[j],self.L[i])
+                    self.E_c[i,j,0] = E_ijk(self.N[i],self.N[j],self.N[j],-hM,hM,self.time,self.L[i],self.L[j])
+                    self.E_c[i,j,1] = E_ijk(self.N[j],self.N[j],self.N[i],-hM,hM,self.time,self.L[j],self.L[j])
         elif method == 'parallel':
-            l1 = Parallel(-1)(delayed(E_ijk)(self.N[i],self.N[j],self.N[j],-hM,hM,-hM,hM,self.time,self.L[i],self.L[j],self.L[j]) for i in range(d) for j in range(d))
-            l2 = Parallel(-1)(delayed(E_ijk)(self.N[j],self.N[j],self.N[i],-hM,hM,-hM,hM,self.time,self.L[j],self.L[j],self.L[i]) for i in range(d) for j in range(d))
+            l1 = Parallel(-1)(delayed(E_ijk)(self.N[i],self.N[j],self.N[j],-hM,hM,self.time,self.L[i],self.L[j]) for i in range(d) for j in range(d))
+            l2 = Parallel(-1)(delayed(E_ijk)(self.N[j],self.N[j],self.N[i],-hM,hM,self.time,self.L[j],self.L[j]) for i in range(d) for j in range(d))
             self.E_c = np.zeros((d,d,2))
             self.E_c[:,:,0] = np.array(l1).reshape(d,d)
             self.E_c[:,:,1] = np.array(l2).reshape(d,d)
@@ -122,7 +122,7 @@ class Cumulants(object):
         assert self.C is not None, "You should first set C using the function 'set_C'."
         assert self.E_c is not None, "You should first set E using the function 'set_E_c'."
         assert self.J is not None, "You should first set J using the function 'set_J'."
-        self.K_c = get_K_c(self.L,self.C,self.J,self.E_c,hM)
+        self.K_c = get_K_c(self.L,self.C,self.J,self.E_c,2*hM)
 
     def set_R_true(self,R_true):
         self.R_true = R_true
@@ -209,7 +209,7 @@ def get_K_c_th(L,C,R):
 #@jit(double(double[:],double[:],int32,int32,double,double,double), nogil=True, nopython=True)
 #@jit(float64(float64[:],float64[:],int64,int64,int64,float64,float64), nogil=True, nopython=True)
 @autojit
-def A_ij(Z_i,Z_j,a,b,T,L_i,L_j):
+def A_ij(Z_i,Z_j,a,b,T,L_j):
     """
     Computes the mean centered number of jumps of N^j between \tau + a and \tau + b, that is
     \frac{1}{T} \sum_{\tau \in Z^i} ( N^j_{\tau + b} - N^j_{\tau + a} - \Lambda^j (b - a) )
@@ -218,9 +218,11 @@ def A_ij(Z_i,Z_j,a,b,T,L_i,L_j):
     u = 0
     n_i = Z_i.shape[0]
     n_j = Z_j.shape[0]
+    trend_j = L_j*(b-a)
     for t in range(n_i):
         # count the number of jumps
         tau = Z_i[t]
+        if tau + a < 0: continue
         while u < n_j:
             if Z_j[u] <= tau + a:
                 u += 1
@@ -233,24 +235,25 @@ def A_ij(Z_i,Z_j,a,b,T,L_i,L_j):
             else:
                 break
         # compute time_window
-        if tau + a < 0:
-            time_window = tau + b
-        elif tau + b > T:
-            time_window = T - (tau + a)
-        else:
-            time_window = b - a
+        #if tau + a < 0:
+        #    time_window = tau + b
+        #elif tau + b > T:
+        #    time_window = T - (tau + a)
+        #else:
+        #    time_window = b - a
         # compute the trend
-        trend_j = L_j*time_window
+        #trend_j = L_j*time_window
+        if v == n_j: continue
         res += v-u-trend_j
     res /= T
     return res
 
 @autojit
-def E_ijk(Z_i,Z_j,Z_k,a_i,b_i,a_j,b_j,T,L_i,L_j,L_k):
+def E_ijk(Z_i,Z_j,Z_k,a,b,T,L_i,L_j):
     """
     Computes the mean of the centered product of i's and j's jumps between \tau + a and \tau + b, that is
-    \frac{1}{T} \sum_{\tau \in Z^k} ( N^i_{\tau + b_i} - N^i_{\tau + a_i} - \Lambda^i * ( b_i - a_i ) )
-                                  * ( N^j_{\tau + b_j} - N^j_{\tau + a_j} - \Lambda^j * ( b_j - a_j ) )
+    \frac{1}{T} \sum_{\tau \in Z^k} ( N^i_{\tau + b} - N^i_{\tau + a} - \Lambda^i * ( b - a ) )
+                                  * ( N^j_{\tau + b} - N^j_{\tau + a} - \Lambda^j * ( b - a ) )
     """
     res = 0
     u = 0
@@ -259,40 +262,40 @@ def E_ijk(Z_i,Z_j,Z_k,a_i,b_i,a_j,b_j,T,L_i,L_j,L_k):
     n_i = Z_i.shape[0]
     n_j = Z_j.shape[0]
     n_k = Z_k.shape[0]
-    trend_i = L_i*(b_i-a_i)
-    trend_j = L_j*(b_j-a_j)
-    C = .5*(A_ij(Z_i,Z_j,-(b_i-a_i),b_i-a_i,T,L_i,L_j)+A_ij(Z_j,Z_i,-(b_i-a_i),b_i-a_i,T,L_j,L_i))
-    J = .5*(I_ij(Z_i,Z_j,(b_i-a_i),T,L_i,L_j)+I_ij(Z_j,Z_i,(b_i-a_i),T,L_j,L_i))
+    trend_i = L_i*(b-a)
+    trend_j = L_j*(b-a)
+    C = .5*(A_ij(Z_i,Z_j,-(b-a),b-a,T,L_j)+A_ij(Z_j,Z_i,-(b-a),b-a,T,L_i))
+    J = .5*(I_ij(Z_i,Z_j,(b-a),T,L_j)+I_ij(Z_j,Z_i,(b-a),T,L_i))
     for t in range(n_k):
         tau = Z_k[t]
-        if tau + a_i < 0: continue
+        if tau + a < 0: continue
         # work on Z_i
         while u < n_i:
-            if Z_i[u] <= tau + a_i:
+            if Z_i[u] <= tau + a:
                 u += 1
             else:
                 break
         v = u
         while v < n_i:
-            if Z_i[v] < tau + b_i:
+            if Z_i[v] < tau + b:
                 v += 1
             else:
                 break
         if v == n_i: continue
         # work on Z_j
         while x < n_j:
-            if Z_j[x] <= tau + a_j:
+            if Z_j[x] <= tau + a:
                 x += 1
             else:
                 break
         y = x
         while y < n_j:
-            if Z_j[y] < tau + b_j:
+            if Z_j[y] < tau + b:
                 y += 1
             else:
                 break
         if y == n_j: continue
-        res += (v-u-trend_i) * (y-x-trend_j) - ((b_i-a_i)*C - 2*J)
+        res += (v-u-trend_i) * (y-x-trend_j) - ((b-a)*C - 2*J)
         # check if this step is admissible
         #if y < n_j and x > 0 and v < n_i and u > 0:
         #    count += 1
@@ -303,13 +306,10 @@ def E_ijk(Z_i,Z_j,Z_k,a_i,b_i,a_j,b_j,T,L_i,L_j,L_k):
     return res
 
 @autojit
-def I_ij(Z_i,Z_j,H,T,L_i,L_j):
+def I_ij(Z_i,Z_j,H,T,L_j):
     """
-
     Computes the integral \int_{(0,H)} t c^{ij} (t) dt. This integral equals
-
-    \frac{1}{T} \sum_{\tau \in Z^i} \sum_{\tau' \in Z^j} (\tau - \tau') 1_{ \tau - H < \tau' < \tau } - H^2 / 2 \Lambda^i \Lambda^j
-
+    \frac{1}{T} \sum_{\tau \in Z^i} \sum_{\tau' \in Z^j} [ (\tau - \tau') 1_{ \tau - H < \tau' < \tau } - H^2 / 2 \Lambda^j ]
     """
     n_i = Z_i.shape[0]
     n_j = Z_j.shape[0]
