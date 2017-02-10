726ee7199a24236a1f439edd3a4c8aa3803a907c
Massil <massil.achab@gmail.com>
2017-02-10 16:37:35 +0100

    main.py is refactored as well

--- nphc/main.py
+++ nphc/main.py
@@ -6,12 +6,11 @@ import tensorflow as tf
 import numpy as np
 
 
-def starting_point(list_cumulants,random=False):
-    if not isinstance(list_cumulants, list): list_cumulants = [list_cumulants]
-    cumulants = list_cumulants[0]
-    d = cumulants.dim
-    sqrt_C = sqrtm(np.mean([cumul.C for cumul in list_cumulants],axis=0))
-    sqrt_L = np.sqrt(np.mean([cumul.L for cumul in list_cumulants],axis=0))
+def starting_point(cumulants_list,random=False):
+    L_list, C_list, K_c_list = cumulants_list
+    d = len(L_list[0])
+    sqrt_C = sqrtm(np.mean(C_list,axis=0))
+    sqrt_L = np.sqrt(np.mean(L_list,axis=0))
     if random:
         M = random_orthogonal_matrix(d)
     else:
@@ -86,13 +85,15 @@ class NPHC(object):
             self.realizations = [realizations]
 
         cumul = Cumulants(realizations, hMax=H)
-        cumul.set_all(H,weight=weight,sigma=H/5.)
+        cumul.compute_cumulants(H,weight=weight,sigma=H/5.)
 
         self.L = cumul.L.copy()
         self.C = cumul.C.copy()
         self.K_c = cumul.K_c.copy()
 
-    def solve(self, initial_point=None):
+
+    def solve(self, initial_point=None, training_epochs=1000, learning_rate=1e6, optimizer='momentum', \
+         display_step = 100):
         """
 
         Parameters
@@ -107,93 +108,81 @@ class NPHC(object):
             optimizer : `str`
                 The optimizer used to minimize the objective function. We use optimizers from TensorFlow.
         """
-        pass
 
+        cumulants_list = [self.L, self.C, self.K_c]
+        d = len(self.L[0])
+        if initial_point is None:
+            start_point = starting_point(cumulants_list, random=False)
+        else:
+            start_point = initial_point.copy()
 
-def NPHC(list_cumulants, initial_point, alpha=.5, training_epochs=1000, learning_rate=1e6, optimizer='momentum', \
-         display_step = 100, l_l1=0., l_l2=0.):
+        R0 = tf.constant(start_point.astype(np.float32), shape=[d,d])
 
-    if not isinstance(list_cumulants, list): list_cumulants = [list_cumulants]
-    cumulants = list_cumulants[0]
+        L = tf.placeholder(tf.float32, d, name='L')
+        C = tf.placeholder(tf.float32, (d,d), name='C')
+        K_c = tf.placeholder(tf.float32, (d,d), name='K_c')
 
-    d = cumulants.dim
+        R = tf.Variable(R0, name='R')
 
-    R0 = tf.constant(initial_point.astype(np.float32), shape=[d,d])
+        # Construct model
+        activation_3 = tf.matmul(C,tf.square(R),transpose_b=True) + 2.0*tf.matmul(R,tf.mul(R,C),transpose_b=True) \
+                       - 2.0*tf.matmul(R,tf.matmul(tf.diag(L),tf.square(R),transpose_b=True))
+        activation_2 = tf.matmul(R,tf.matmul(tf.diag(L),R,transpose_b=True))
 
-    L = tf.placeholder(tf.float32, d, name='L')
-    C = tf.placeholder(tf.float32, (d,d), name='C')
-    K_c = tf.placeholder(tf.float32, (d,d), name='K_c')
+        cost =  (1-self.alpha) * tf.reduce_mean( tf.squared_difference( activation_3, K_c ) ) \
+        + self.alpha * tf.reduce_mean( tf.squared_difference( activation_2, C ) )
 
-    R = tf.Variable(R0, name='R')
+        reg_l1 = tf.contrib.layers.l1_regularizer(l_l1)
+        reg_l2 = tf.contrib.layers.l2_regularizer(l_l2)
+        if l_l1*l_l2 > 0:
+            cost = tf.cast(cost, tf.float32) + reg_l1(R) + reg_l2(R)
+        else:
+            cost = tf.cast(cost, tf.float32)
 
-    # Set weight matrices
-    W_2 = np.ones((d,d))
-    W_3 = np.ones((d,d))
+        if optimizer == 'momentum':
+            optimizer = tf.train.MomentumOptimizer(learning_rate, momentum=0.9).minimize(cost)
+        elif optimizer == 'adam':
+            optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+        elif optimizer == 'adagrad':
+            optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(cost)
+        elif optimizer == 'rmsprop':
+            optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+        elif optimizer == 'adadelta':
+            optimizer = tf.train.AdadeltaOptimizer(learning_rate).minimize(cost)
+        else:
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-    # Construct model
-    activation_3 = tf.matmul(C,tf.square(R),transpose_b=True) + 2.0*tf.matmul(R,tf.mul(R,C),transpose_b=True) - 2.0*tf.matmul(R,tf.matmul(tf.diag(L),tf.square(R),transpose_b=True))
-    activation_2 = tf.matmul(R,tf.matmul(tf.diag(L),R,transpose_b=True))
+        # Initialize the variables
+        init = tf.initialize_all_variables()
 
-    #cost_3 = tf.reduce_mean( tf.squared_difference( activation_3, K_c ) )
-    #cost_2 = tf.reduce_mean( tf.squared_difference( activation_2, C ) )
+        # Launch the graph
+        with tf.Session() as sess:
+            sess.run(init)
 
-    cost =  (1-alpha) * tf.reduce_mean( tf.squared_difference( activation_3, K_c ) ) + alpha * tf.reduce_mean( tf.squared_difference( activation_2, C ) )
+            # Set logs writer into folder /tmp/tf_cumul
+            #summary_writer = tf.train.SummaryWriter('/tmp/tf_cumul', graph=sess.graph)
 
-    reg_l1 = tf.contrib.layers.l1_regularizer(l_l1)
-    reg_l2 = tf.contrib.layers.l2_regularizer(l_l2)
-    if l_l1*l_l2 > 0:
-        cost = tf.cast(cost, tf.float32) + reg_l1(R) + reg_l2(R)
-    else:
-        cost = tf.cast(cost, tf.float32)
+            # Training cycle
+            for epoch in range(training_epochs):
 
-    if optimizer == 'momentum':
-        optimizer = tf.train.MomentumOptimizer(learning_rate, momentum=0.9).minimize(cost)
-    elif optimizer == 'adam':
-        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-    elif optimizer == 'adagrad':
-        optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(cost)
-    elif optimizer == 'rmsprop':
-        optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
-    elif optimizer == 'adadelta':
-        optimizer = tf.train.AdadeltaOptimizer(learning_rate).minimize(cost)
-    else:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+                cumulants = np.random.choice(list_cumulants)
 
-    # Initialize the variables
-    init = tf.initialize_all_variables()
+                if epoch % display_step == 0:
+                    avg_cost = np.average([sess.run(cost, feed_dict={L: cumul.L, C: cumul.C, K_c: cumul.K_c})
+                                           for cumul in list_cumulants])
+                    print("Epoch:", '%04d' % (epoch), "log10(cost)=", "{:.9f}".format(np.log10(avg_cost)))
 
-    # Create a summary to monitor cost function
-    #tf.scalar_summary('loss', cost)
+                # Fit training using batch data
+                sess.run(optimizer, feed_dict={L: cumulants.L, C: cumulants.C, K_c: cumulants.K_c})
 
-    # Merge all summaries to a single operator
-    #merged_summary_op = tf.merge_all_summaries()
+                # Write logs at every iteration
+                #summary_str = sess.run(merged_summary_op, feed_dict={L: cumul.L, C: cumul.C, K_c: cumul.K_c})
+                #summary_writer.add_summary(summary_str, epoch)
 
-    # Launch the graph
-    with tf.Session() as sess:
-        sess.run(init)
+            print("Optimization Finished!")
 
-        # Set logs writer into folder /tmp/tf_cumul
-        #summary_writer = tf.train.SummaryWriter('/tmp/tf_cumul', graph=sess.graph)
+            return sess.run(R)
 
-        # Training cycle
-        for epoch in range(training_epochs):
-
-            cumulants = np.random.choice(list_cumulants)
-
-            if epoch % display_step == 0:
-                avg_cost = np.average([sess.run(cost, feed_dict={L: cumul.L, C: cumul.C, K_c: cumul.K_c}) for cumul in list_cumulants])
-                print("Epoch:", '%04d' % (epoch), "log10(cost)=", "{:.9f}".format(np.log10(avg_cost)))
-
-            # Fit training using batch data
-            sess.run(optimizer, feed_dict={L: cumulants.L, C: cumulants.C, K_c: cumulants.K_c})
-
-            # Write logs at every iteration
-            #summary_str = sess.run(merged_summary_op, feed_dict={L: cumul.L, C: cumul.C, K_c: cumul.K_c})
-            #summary_writer.add_summary(summary_str, epoch)
-
-        print("Optimization Finished!")
-
-        return sess.run(R)
 
 '''
 Run the command line: tensorboard --logdir=/tmp/tf_cumul
